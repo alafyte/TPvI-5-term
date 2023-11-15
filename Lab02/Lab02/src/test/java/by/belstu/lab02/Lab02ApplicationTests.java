4ae29942a55010ccb97fb893bcacadc9c3754fc5
alafyte <xleb.xlebnyj@mail.ru>
2023-11-15 16:55:18 +0300

    project configured

---
+++ Lab02/Lab02/src/test/java/by/belstu/lab02/Lab02ApplicationTests.java
@@ -0,0 +1,13 @@
+package by.belstu.lab02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
