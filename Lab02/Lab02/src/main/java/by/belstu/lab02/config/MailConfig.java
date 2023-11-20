fd7f8c2777d487b18b23ca602c6480aa62805289
alafyte <xleb.xlebnyj@mail.ru>
2023-11-20 15:19:27 +0300

    Mail sender has been configured

---
+++ Lab02/Lab02/src/main/java/by/belstu/lab02/config/MailConfig.java
@@ -0,0 +1,54 @@
+package by.belstu.lab02.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+    private String starttls;
+
+    @Value("${spring.mail.transport.protocol}")
+    private String protocol;
+
+    @Value("${spring.mail.debug}")
+    private String debug;
+
+    @Value("${spring.mail.smtp.ssl.trust}")
+    private String trust;
+
+    @Bean
+    public JavaMailSender getMailSender(){
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties properties = mailSender.getJavaMailProperties();
+
+        properties.setProperty("mail.transport.protocol", protocol);
+        properties.setProperty("mail.debug", debug);
+        properties.setProperty("mail.smtp.ssl.trust", trust);
+
+        properties.setProperty("mail.smtp.starttls.enable", starttls);
+        return  mailSender;
+    }
+}
